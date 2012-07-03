06e32279c60b36bda45328b8e50a46bda3f82c64
Gene McCulley <mcculley@stackframe.com>
2012-07-03 10:36:39 -0400

    Add copyright prologue.

--- src/java/com/stackframe/symbolfactory/milstd2525b/SymbolFactory2525B.java
+++ src/java/com/stackframe/symbolfactory/milstd2525b/SymbolFactory2525B.java
@@ -1,6 +1,17 @@
 /*
- * Copyright 2011 StackFrame, LLC
- * All rights reserved.
+ * Copyright (C) 2011-2012 StackFrame, LLC
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 2 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. For all other purposes, contact sales@stackframe.com for a license.
+ * 
+ * You should have received a copy of version 2 of the GNU General Public
+ * License along with this program; if not, contact info@stackframe.com.
  */
 package com.stackframe.symbolfactory.milstd2525b;
 
